9ff3b5fcfb551df4e49758b64dd38d807daf4b63
zonghan <steven.me05@nctu.edu.tw>
2020-03-27 02:43:11 +0800

    [HW1] implement expand function & write the move rule.

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
 
 description = """This is Introduction to AI course homework 1.
-you can input the type of algorithm list below :
+you should input the type of algorithm list below :
     0  ---  BFS
     1  ---  DFS
     2  ---  IDS
@@ -10,7 +10,7 @@ you can input the type of algorithm list below :
 print(description)
 
 parser = ArgumentParser()
-parser.add_argument("algo_num", type=int, help="choose a algorithm.")
+parser.add_argument("num", type=int, help="choose a algorithm.")
 parser.add_argument("sx", type=int, help="assign starting x.")
 parser.add_argument("sy", type=int, help="assign starting x.")
 parser.add_argument("gx", type=int, help="assign goal x.")
@@ -21,27 +21,70 @@ args = parser.parse_args()
 class ChessBoard():
     def __init__(self, args):
         self.algo = args.algo_num
-        self.sx = args.sx
-        self.sy = args.sy
+        self.cur_x = args.sx
+        self.cur_y = args.sy
         self.gx = args.gx
         self.gy = args.gy
         self.explored = set()
-        self.frontier = []
+        self.frontier = [(args.sx, args.sy)]
+        self.path = dict()
 
-    def BFS():
+    def BFS(self):
         pass
 
-    def DFS():
+    def DFS(self):
         pass
 
-    def IDS():
+    def IDS(self):
         pass
 
-    def A_star():
+    def A_star(self):
         pass
 
-    def IDA_star():
+    def IDA_star(self):
         pass
+
+    def expand(self):
+        self.cur_x, self.cur_y = self.frontier[0][0], self.frontier[0][1]
+
+        if self.allow((self.cur_x + 1, self.cur_y + 2)):
+            self.frontier.append((self.cur_x + 1, self.cur_y + 2))
+            self.path[(self.cur_x + 1, self.cur_y + 2)] = (self.cur_x, self.cur_y)
+        if self.allow((self.cur_x + 1, self.cur_y - 2)):
+            self.frontier.append((self.cur_x + 1, self.cur_y - 2))
+            self.path[(self.cur_x + 1, self.cur_y - 2)] = (self.cur_x, self.cur_y)
+        if self.allow((self.cur_x - 1, self.cur_y + 2)):
+            self.frontier.append((self.cur_x - 1, self.cur_y + 2))
+            self.path[(self.cur_x - 1, self.cur_y + 2)] = (self.cur_x, self.cur_y)
+        if self.allow((self.cur_x - 1, self.cur_y - 2)):
+            self.frontier.append((self.cur_x - 1, self.cur_y - 2))
+            self.path[(self.cur_x - 1, self.cur_y - 2)] = (self.cur_x, self.cur_y)
+        if self.allow((self.cur_x + 2, self.cur_y + 1)):
+            self.frontier.append((self.cur_x + 2, self.cur_y + 1))
+            self.path[(self.cur_x + 2, self.cur_y + 1)] = (self.cur_x, self.cur_y)
+        if self.allow((self.cur_x + 2, self.cur_y - 1)):
+            self.frontier.append((self.cur_x + 2, self.cur_y - 1))
+            self.path[(self.cur_x + 2, self.cur_y - 1)] = (self.cur_x, self.cur_y)
+        if self.allow((self.cur_x - 2, self.cur_y + 1)):
+            self.frontier.append((self.cur_x - 2, self.cur_y + 1))
+            self.path[(self.cur_x - 2, self.cur_y + 1)] = (self.cur_x, self.cur_y)
+        if self.allow((self.cur_x - 2, self.cur_y - 1)):
+            self.frontier.append((self.cur_x - 2, self.cur_y - 1))
+            self.path[(self.cur_x - 2, self.cur_y - 1)] = (self.cur_x, self.cur_y)
+
+        self.explored.add(self.frontier[0])
+        del self.frontier[0]
+
+    def allow(self, pos):
+        pos[0], pos[1] = x, y
+        if x > 7 or x < 0:
+            return False
+        elif y > 7 or y < 0:
+            return False
+        elif (x, y) in self.explored:
+            return False
+        else:
+            return True
 
 if __name__ == "__main__":
     chessboard = ChessBoard(args)
