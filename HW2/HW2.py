c92f1aa5b413fe70894d13b87059da513d976fbe
zonghan <steven.me05@nctu.edu.tw>
2020-05-01 15:27:24 +0800

    [HW2] implement in class.

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -2,124 +2,153 @@ import sys
 import time
 import numpy as np
 
-argc = len(sys.argv)
+class Minesweeper():
+    def __init__(self):
+        try:
+            self.argc = len(sys.argv)
+            self.size_x = int(sys.argv[1])
+            self.size_y = int(sys.argv[2])
+            self.mines = int(sys.argv[3])
+        except:
+            pass
 
-if (argc < 4):
-    print("[ERROR] input format error")
-    sys.exit(-1)
+        ls = []
 
-size_x = int(sys.argv[1])
-size_y = int(sys.argv[2])
-mines = int(sys.argv[3])
+        for i in range(4, self.argc):
+            ls.append(int(sys.argv[i]))
 
-if (argc < 4 + size_x * size_y):
-    print("[ERROR] board not complete")
-    sys.exit(0)
+        self.board = np.array(ls).reshape(self.size_y, self.size_x)
+        self.copy = np.array(self.board, copy = True)
+        self.found = False
 
-ls = []
-
-for i in range(4, argc):
-    ls.append(int(sys.argv[i]))
-
-board = np.array(ls).reshape(size_y, size_x)
-copy = np.array(board, copy = True)
-found = False
-
-def minetest(bd, x, y):
-    limit = bd[y][x]
-    cnt = 0
-    for i in range(x-1, x+2):
-        for j in range(y-1, y+2):
-            if (i < 0 or i >= size_x):
-                continue
-            if (j < 0 or j >= size_y):
-                continue
-            if (bd[j][i] == -2):
-                cnt += 1
-            if (cnt > limit):
-                break
-    if (cnt != limit):
-        return False
-    else:
-        return True
-
-def countmine(bd):
-    count = 0
-    for i in range(size_x):
-        for j in range(size_y):
-            if (bd[j][i] == -2):
-                count += 1
-
-    return count
-
-
-def check(bd):
-    for j in range(size_y):
-        for i in range(size_x):
-            if (bd[j][i] >= 0):
-                ret = minetest(bd, i, j)
-                if (ret == False):
+    def minetest(self, bd, x, y):
+        limit = bd[y][x]
+        cnt = 0
+        for i in range(x-1, x+2):
+            for j in range(y-1, y+2):
+                if (i < 0 or i >= self.size_x):
+                    continue
+                if (j < 0 or j >= self.size_y):
+                    continue
+                if (bd[j][i] == -2):
+                    cnt += 1
+                if (cnt > limit):
                     return False
-    return True
+        if (cnt != limit):
+            return False
+        else:
+            return True
+
+    def countmine(self, bd):
+        count = 0
+        for i in range(self.size_x):
+            for j in range(self.size_y):
+                if (bd[j][i] == -2):
+                    count += 1
+
+        return count
 
 
-def backtrack(bd, x, y):
-    global found
-    if found:
+    def check(self, bd):
+        for j in range(self.size_y):
+            for i in range(self.size_x):
+                if (bd[j][i] >= 0):
+                    ret = self.minetest(bd, i, j)
+                    if (ret == False):
+                        return False
         return True
-    if (x >= size_x or y >= size_y):
-        ret = check(bd)
-        if (ret == True and countmine(bd) == mines):
-            global ans
-            ans = np.array(bd, copy = True)
-            found = True
-        return
 
-    child = np.array(bd, copy = True)
-    if (board[y][x] == -1 and countmine(bd) < mines):
-        child[y][x] = -2
-        print(child)
-    new_x = (x + 1) % size_x
-    new_y = y
-    if (new_x == 0):
-        new_y += 1
+    def forward_check(self, bd):
+        for y in range(self.size_y):
+            for x in range(self.size_x):
+                if (bd[x][y] >= 0):
+                    for i in range(x-1, x+2):
+                        for j in range(y-1, y+2):
+                            if (i < 0 or i >= self.size_x):
+                                continue
+                            if (j < 0 or j >= self.size_y):
+                                continue
+                            if (bd[j][i] == -2):
+                                cnt += 1
+                            if (cnt > limit):
+                                return False
+        return True
 
-    if (new_y < size_y and new_x < size_x):
-        while (board[new_y][new_x] != -1):
-            new_x = (new_x + 1) % size_x
-            new_y = new_y
-            if (new_x == 0):
-                new_y += 1
-            if (new_y >= size_y or new_x >= size_x):
-                break
-    backtrack(child, new_x, new_y)
-    if (board[y][x] == -1):
-        child[y][x] = -5
-        print(child)
-    backtrack(child, new_x, new_y)
 
-def printboard():
-    for j in range(size_y):
-        for i in range(size_x):
-            if (ans[j][i] == -2):
-                print(" * ", end = "")
-            elif (ans[j][i] == -5 or ans[j][i] == -1):
-                print(" ~ ", end = "")
-            else:
-                print(" %d "%ans[j][i], end = "")
-        print("")
+    def backtrack(self, bd, x, y):
+        if self.found:
+            return True
+        if (x >= self.size_x or y >= self.size_y):
+            ret = self.check(bd)
+            if (ret == True and self.countmine(bd) == self.mines):
+                self.ans = np.array(bd, copy = True)
+                self.found = True
+            return
 
-time1 = time.time()
-backtrack(copy, 0, 0)
-time2 = time.time()
-elapse = time2 - time1
+        child = np.array(bd, copy = True)
+        if (self.board[y][x] == -1 and self.countmine(bd) < self.mines):
+            child[y][x] = -2
+            print(child)
+        new_x = (x + 1) % self.size_x
+        new_y = y
+        if (new_x == 0):
+            new_y += 1
 
-try:
-    printboard()
-    print("elapsed time: %ss"%str(elapse))
-except:
-    print("no solution")
+        if (new_y < self.size_y and new_x < self.size_x):
+            while (self.board[new_y][new_x] != -1):
+                new_x = (new_x + 1) % self.size_x
+                new_y = new_y
+                if (new_x == 0):
+                    new_y += 1
+                if (new_y >= self.size_y or new_x >= self.size_x):
+                    break
+        self.backtrack(child, new_x, new_y)
+        if (self.board[y][x] == -1):
+            child[y][x] = -5
+            print(child)
+        self.backtrack(child, new_x, new_y)
 
+    def printboard(self):
+        print("\n==================================\n")
+        print("Sample solution:")
+        for j in range(self.size_y):
+            print("----" * self.size_x)
+            for i in range(self.size_x):
+                if (i == 0):
+                    print("|", end = "")
+                if (self.ans[j][i] == -2):
+                    print(" * ", end = "|")
+                elif (self.ans[j][i] == -5 or self.ans[j][i] == -1):
+                    print(" ~ ", end = "|")
+                else:
+                    print(" %d "%self.ans[j][i], end = "|")
+            print("")
+        print("----" * self.size_x)
+
+
+
+if __name__ == "__main__":
+    minesweeper = Minesweeper()
+    if (minesweeper.argc < 4):
+        print("[ERROR] input format error")
+        sys.exit(-1)
+
+
+    if (minesweeper.argc < 4 + minesweeper.size_x * minesweeper.size_y):
+        print("[ERROR] board not complete")
+        sys.exit(0)
+
+    time1 = time.time()
+    minesweeper.backtrack(minesweeper.copy, 0, 0)
+    time2 = time.time()
+    elapse = time2 - time1
+
+    try:
+        minesweeper.printboard()
+    except:
+        print("no solution")
+
+    print("\nelapsed time: %s (s)"%str(elapse))
 ##
 ## test = np.array([-1,-2,-1,1,1,-1,
 ##                  -2,3,-1,-2,-1,0,
