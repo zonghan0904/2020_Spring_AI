abdd609baeacac565272c415e87762718b0bd4ec
zonghan <steven.me05@nctu.edu.tw>
2020-05-20 19:21:07 +0800

    [HW3] initialize.

---
+++ HW3/HW3.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+class Minesweeper():
+    '''
+    game control module
+    '''
+    def __init__(self):
+        pass
+    def gen_board(self):
+        pass
+    def hint(self):
+        pass
+    def ini_safe(self):
+        pass
+
+class player():
+    '''
+    player module
+    '''
+    def __init__(self):
+        pass
+    def mark(self):
+        pass
+    def matching(self):
+        pass
+    def insert(self):
+        pass
+    def query(self):
+        pass
+    def gen_clauses(self):
+        pass
+
+
